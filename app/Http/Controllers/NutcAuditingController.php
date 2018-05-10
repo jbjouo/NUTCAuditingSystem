9493a8d30187542118058682cdb19878f2772563
BrockGun <34338728+BrockGun@users.noreply.github.com>
2018-05-11 01:21:18 +0800

    test git work

--- app/Http/Controllers/NutcAuditingController.php
+++ app/Http/Controllers/NutcAuditingController.php
@@ -8,6 +8,7 @@ use Auth;
 class NutcAuditingController extends Controller
 {
 	public function index(){
+				$a=1;
         $user = Auth::user();
         return view('NutcAuditing.index',['user'=>$user]);
 	}
