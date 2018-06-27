ed9d08f82442440c7de06973d22fd710f86b844e
BrockGun <34338728+BrockGun@users.noreply.github.com>
2018-06-27 16:16:29 +0800

    pdf project

--- app/Http/Controllers/pdfController.php
+++ app/Http/Controllers/pdfController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use PDF;
+use App\Project;
 class pdfController extends Controller
 {
     public function pdftest(){
+      $data=Project::where('id','2')->get();
+      PDF::SetFont('stsongstdlight', '', 14);
       PDF::SetTitle('Hello World');
-      PDF::AddPage();
-      PDF::Write(0, 'Hello World');
+      PDF::AddPage('P');
+      PDF::WriteHTML(view('pdf.project',['data'=>$data])->render());
       PDF::Output('hello_world.pdf');
+      //return view('pdf.project',['data'=>$data]);
     }
 }
---
+++ resources/views/pdf/project.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='utf8'/>
+  <style>
+    body{ margin:0px auto;}
+    p{width: 10cm;margin: 0px auto;}
+    h2{text-align:center;}
+  </style>
+</head>
+<body>
+<h2>國立台中科技大學</h1>
+<h2>______年度內部稽核計畫</h2>
+<p >壹、稽核範圍</p>
+<p>{!!html_entity_decode($data[0]->Audit_scope)!!}</p>
+<p >貳、稽核重點(目的)</p>
+<p>{!!html_entity_decode($data[0]->Audit_focus)!!}</p>
+<p >參、稽核項目及期程(詳見內部稽核計畫表)</p>
+</body>
+</html>
