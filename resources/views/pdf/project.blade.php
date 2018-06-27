08f1ed7aff024ba84397a0e62ace93773d624461
BrockGun <34338728+BrockGun@users.noreply.github.com>
2018-06-27 16:41:17 +0800

    add image

--- resources/views/pdf/project.blade.php
+++ resources/views/pdf/project.blade.php
@@ -3,12 +3,14 @@
 <head>
   <meta charset='utf8'/>
   <style>
-    body{ margin:0px auto;}
-    p{width: 10cm;margin: 0px auto;}
-    h2{text-align:center;}
+  body{ margin:0px auto;}
+  p{width: 10cm;margin: 0px auto;}
+  h2{text-align:center;}
+  img{float:right;}
   </style>
 </head>
 <body>
+<img src="{{asset('/images/logo.gif')}}" width="130" >
 <h2>國立台中科技大學</h1>
 <h2>______年度內部稽核計畫</h2>
 <p >壹、稽核範圍</p>
